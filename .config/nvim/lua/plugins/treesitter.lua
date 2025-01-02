034c1f78f1a6d2a05525a3780b2c1e2eaccb8100
Jasper De Langhe <25288931+JasperDeLanghe@users.noreply.github.com>
2025-01-02 10:36:32 +0100

    nvim config

---
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,29 @@
+-- treesitter.lua
+require("lazy").setup({
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+      local configs = require("nvim-treesitter.configs")
+
+      configs.setup({
+        ensure_installed = {
+          "c",
+          "lua",
+          "vim",
+          "vimdoc",
+          "query",
+          "python",
+          "typescript",
+          "javascript",
+          "html",
+          "css",
+          "scss",
+        },
+        sync_install = false,
+        highlight = { enable = true },
+        indent = { enable = true },
+      })
+    end,
+  },
+})
